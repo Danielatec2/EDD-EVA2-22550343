8a76031168a81d32dd10db7ad3a900fcc1009e42
Daniela Piñón Polanco <124320997+Danielatec2@users.noreply.github.com>
2023-03-13 20:31:58 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_HERENCIA/src/eva2_5_tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_5_tipos_herencia;
+
+/**
+ *
+ * @author danii
+ */
+public class EVA2_5_TIPOS_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //TENEMOS DOS TIPOS DE HERENCIA 
+    //MULTIPLE Y SIMOLE
+    //JAVA SOLO PERMITE SIMPLE , PEROOOO PERMITE SIMULAR HERENCIA MULTIPLE CON EL USO DE INTERFACES
+    // UN PROBLEMA DE LA HERENCIA SON LAS COLICIONES ELEMENTOS 
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    class SuperClase{
+        
+    }
+    class OtraSuperClase{
+        //NO SE HEREDA DE DOS O MAS SUPERCLASES
+        
+    }
+    class BaseClase extends SuperClase{
+        //EXTENDS SOLO FUNCIONA PARA UNA UNICA CLASE 
+        
+    }
+}
