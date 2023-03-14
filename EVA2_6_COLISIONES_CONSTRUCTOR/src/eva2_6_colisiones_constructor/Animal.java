dd02b93fdd4f22b045d742de188424e2cb7fbc7a
Daniela Piñón Polanco <124320997+Danielatec2@users.noreply.github.com>
2023-03-13 20:34:25 -0600

    Add files via upload

---
+++ EVA2_6_COLISIONES_CONSTRUCTOR/src/eva2_6_colisiones_constructor/Animal.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_6_colisiones_constructor;
+
+/**
+ *
+ * @author danii
+ */
+public class Animal {
+
+    public Animal() {
+        System.out.println("ESTOY VIVO!!!");
+       
+    }
+    
+    public void comer(){
+        System.out.println("Estoy comiendo!!!");
+    
+}
+    public void respirar(){
+        System.out.println("Estoy respirando!!");
+    }
+    
+            
+            
+    
+}
